acbe0fb92537c7b5ad0badbc7ee4af9eee988143
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-18 15:04:31 +0300

    Added Tast19

---
+++ Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string Number = Console.ReadLine();
+int RealNumber;
+bool IsNumber = int.TryParse(Number, out RealNumber);
+if (IsNumber)
+{
+    if (RealNumber < 0)
+    {
+        RealNumber = RealNumber * (-1);
+    }
+    if ((RealNumber >= 10000) && (RealNumber <= 99999))
+    {
+        if ((RealNumber / 10000) == (RealNumber % 10) && ((RealNumber / 1000) % 10) == ((RealNumber % 100) / 10))
+        {
+            Console.WriteLine($"Число {Number} является полиндромом");
+        }
+        else Console.WriteLine($"Число {Number} не является полиндромом");
+    }
+    else Console.WriteLine($"Число {Number} не является пятизначным. Введите корректное число");
+}
+else Console.WriteLine($"Число {Number} состоит не только из цифр");
