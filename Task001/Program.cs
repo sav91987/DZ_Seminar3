1bbb31a35a17e02eca04adbe4138a82587ffdc4f
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-18 16:15:35 +0300

    Added Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите сумму вклада: ");
+int Sum = Convert.ToInt32(Console.ReadLine());
+
+double ResultSum = Sum;
+
+if ((Sum <= 100) && (Sum > 0))
+{
+    ResultSum = Sum + Sum * 0.05;
+    Console.WriteLine(ResultSum);
+}
+else if ((Sum > 100) && (Sum <= 200))
+{
+    ResultSum = Sum + Sum * 0.07;
+    Console.WriteLine(ResultSum);
+}
+else if (Sum > 200)
+{
+    ResultSum = Sum + Sum * 0.1;
+    Console.WriteLine(ResultSum);
+}
+else Console.WriteLine("Введите корректную сумму вклада");
