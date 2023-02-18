f48f1e3218c011070f7d814c8101553621b05231
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-18 15:42:42 +0300

    Added Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 0) N = N * (-1);
+for (int i = 1; i <= N; i++)
+{
+    Console.Write(Math.Pow(i, 3) + "\t");
+}
