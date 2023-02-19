676427e7bdd1ad09a8aecc030f04aeb7e0d8978f
Sotnikov_Andrey <sav91987@ya.ru>
2023-02-19 18:39:55 +0300

    Added Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Введите число: ");
+string Str = Console.ReadLine();
+
+int Number = Convert.ToInt32(Str);
+
+if (Number < 0) Number = Number * (-1);
+
+if (Number < 100) Console.WriteLine("Число двузначное: середины нет");
+else
+{
+    int NumberSaved = Number;
+    int count = 0;
+    int Max = Number % 10;
+    int Min = Number % 10;
+    while (Number > 0)
+    {
+        if ((Number % 10) > Max)
+        {
+            Max = Number % 10;
+        }
+        else if ((Number % 10) < Min)
+        {
+            Min = Number % 10;
+        }
+        Number /= 10;
+        count++;
+    }
+
+    if ((count % 2) == 0)
+    {
+        Console.WriteLine("У данного числа нет середины");
+    }
+    else
+    {
+        int SrNumber = 0;
+        for (int i = 1; i <= (count / 2); i++)
+        {
+            NumberSaved /= 10;
+            SrNumber = NumberSaved % 10;
+        }
+        if ((Max - Min) == SrNumber)
+        {
+            Console.WriteLine("Интересное");
+        }
+        else Console.WriteLine("Не интересное");
+    }
+}
